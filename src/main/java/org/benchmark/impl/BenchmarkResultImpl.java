6758d9ddd599a991216835840f8196896698edeb
Filippo Sanzani <filippo.sanzani@studio.unibo.it>
2022-12-10 14:11:38 +0100

    Implement `BenchmarkResult`.

---
+++ src/main/java/org/benchmark/impl/BenchmarkResultImpl.java
@@ -0,0 +1,49 @@
+package org.benchmark.impl;
+
+import org.benchmark.BenchmarkResult;
+import org.benchmark.OperationType;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.DoubleStream;
+
+public class BenchmarkResultImpl implements BenchmarkResult {
+    private final List<Measurement> measurements
+        = new ArrayList<>();
+
+    public BenchmarkResultImpl() {
+
+    }
+
+    @Override
+    public void addMeasurement(Measurement measurement) {
+        this.measurements.add(measurement);
+    }
+
+    @Override
+    public double getAvgExecutionTimeForOperation(OperationType operation) {
+        return this.getMeasurementsAsFilteredDoubleStream(operation)
+                .average()
+                .getAsDouble();
+    }
+
+    @Override
+    public double getMinExecutionTimeForOperation(OperationType operation) {
+        return this.getMeasurementsAsFilteredDoubleStream(operation)
+                .min()
+                .getAsDouble();
+    }
+
+    @Override
+    public double getMaxExecutionTimeForOperation(OperationType operation) {
+        return this.getMeasurementsAsFilteredDoubleStream(operation)
+                .max()
+                .getAsDouble();
+    }
+
+    private DoubleStream getMeasurementsAsFilteredDoubleStream(OperationType operation) {
+        return measurements.stream()
+            .filter(operation::equals)
+            .mapToDouble(Measurement::getValue);
+    }
+}
